f0b14372706380a34136025de31dcd69d7dd51a9
Nekoleev <133821916+nekoleev@users.noreply.github.com>
2024-06-08 21:07:49 +0800

    finish foundation for projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <>
       <header className="h-14 px-4 flex justify-between items-center">
-        <h1 className="text-xl">P R O J E C T - M I N I E S</h1>
+        <h1 className="text-sm">P R O J E C T - M I N I E S</h1>
         <ModeToggle/>
       </header>
       <main className="flex-grow flex justify-center items-center border-2 border-solid border-white">
         <p>Main</p>
       </main>
       <footer className="h-10 flex justify-center items-center">
-        <p>Built by Nekoleev.dev</p>
+        <p className="text-sm">Built by Nekoleev.dev</p>
       </footer>
     </>
   );
